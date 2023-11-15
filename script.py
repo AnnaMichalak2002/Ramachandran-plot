4ef2465b7ac25a5994145920cadfbd9c9908a446
AnnaMichalak2002 <127102554+AnnaMichalak2002@users.noreply.github.com>
2023-11-15 21:46:39 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,65 +1,79 @@
+import sys
 import numpy as np
-from Bio import PDB
-import matplotlib.pyplot as plt
-from Bio.PDB import Polypeptide
-from Bio.PDB.DSSP import DSSP
+from Bio import PDB     # do obliczania kątów phi i psi
+import matplotlib.pyplot as plt  # do rysowania Ramachandran Plot
+from Bio.PDB import Polypeptide # stąd  informacje o kątach torsyjnych (z obiektu Polypeptide)
+from Bio.PDB.DSSP import DSSP  # do uzyskania informacji o strukturze wtórnej.
 
-def calculate_phi_psi(structure, pdb_filename):
+def calculate_phi_psi(structure):
+    # Inicjalizacja list do przechowywania kątów phi, psi i struktury wtórnej
     phi_angles = []
     psi_angles = []
     secondary_structure = []
 
-    for model in structure:
-        for chain in model:
-            polypeptides = Polypeptide.Polypeptide(chain)
-            phi_psi_angles = polypeptides.get_phi_psi_list()
-            dssp = PDB.DSSP(model, pdb_filename)
+    for model in structure:      # Iteracja po modelach w strukturze białka
+        for chain in model:      # Iteracja po łańcuchach w każdym modelu
+            polypeptides = Polypeptide.Polypeptide(chain)    # Utworzenie obiektu Polypeptide dla danego łańcucha -  klasy, która umożliwia analizę sekwencji aminokwasów w ramach łańcucha białkowego.
+            phi_psi_angles = polypeptides.get_phi_psi_list() # Pobranie listy kątów phi i psi dla danego łańcucha
+            dssp = PDB.DSSP(model, pdb_filename)  #Utworzenie obiektu DSSP dla danego modelu do analizy struktury wtórnej (jak helisy alfa, arkusze beta, zgięcia itp.)
 
-            residues = list(chain.get_residues())
-            for phi, psi in phi_psi_angles:
-                if phi is not None and psi is not None:
-                    residue_index = polypeptides.get_phi_psi_list().index((phi, psi))
-                    residue_id = (chain.id, residues[residue_index].get_id()[1])
-                    phi_angles.append(np.degrees(phi))
+            residues = list(chain.get_residues()) # uzyskujemy pełną listę reszt aminokwasowych w danym łańcuchu
+            for phi, psi in phi_psi_angles:       #  Iteracja po parach kątów phi i psi
+                if phi is not None and psi is not None: # Sprawdzenie, czy oba kąty są zdefiniowane
+                    residue_index = polypeptides.get_phi_psi_list().index((phi, psi)) # Indeks reszty aminokwasowej w Polypeptide
+                    residue_id = (chain.id, residues[residue_index].get_id()[1]) #tworzenia identyfikatora reszty aminokwasowej, który składa się z identyfikatora łańcucha oraz numeru reszty
+                    phi_angles.append(np.degrees(phi))              # Dodanie kątów phi i psi w stopniach do list
                     psi_angles.append(np.degrees(psi))
-                    secondary_structure.append(dssp[residue_id][2])
+                    secondary_structure.append(dssp[residue_id][2])  # Dodanie informacji o strukturze wtórnej do listy
 
-    return phi_angles, psi_angles, secondary_structure
+    return phi_angles, psi_angles, secondary_structure  # Zwrócenie list kątów phi, psi i struktury wtórnej
 
 def plot_ramachandran(phi_angles, psi_angles, secondary_structure):
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(8, 6))  # Utworzenie nowego obiektu figury o rozmiarze 8x6 cali
     
-    # Map secondary structure types to colors
+    # Mapowanie typów struktury wtórnej na kolory
     color_map = {'H': 'red', 'E': 'blue', '-': 'green', 'G': 'orange', 'T': 'purple', 'S': 'yellow', 'I': 'cyan', 'B': 'pink'}
 
-    # Scatter points with color based on secondary structure
+    # Rozrzucanie punktów na wykresie, kolorując je w zależności od struktury wtórnej
     for phi, psi, ss in zip(phi_angles, psi_angles, secondary_structure):
-        plt.scatter(phi, psi, s=10, alpha=0.5, edgecolors='none', c=color_map[ss])
+        plt.scatter(phi, psi, s=10, alpha=0.5, edgecolors='none', c=color_map[ss]) #Dodaje punkt na wykresie.
 
-    plt.title('Ramachandran Plot with Secondary Structure Coloring')
+    plt.title('Ramachandran Plot with Secondary Structure Coloring')  # Dodanie tytułu i opisów osi
     plt.xlabel('Phi Angle (degrees)')
     plt.ylabel('Psi Angle (degrees)')
-    plt.xlim(-180, 180)
+    plt.xlim(-180, 180)                                               # Ustawienie zakresów osi
     plt.ylim(-180, 180)
-    plt.axhline(0, color='black', linewidth=0.5)
+    plt.axhline(0, color='black', linewidth=0.5)                       # Dodanie linii odniesienia w poziomie i pionie
     plt.axvline(0, color='black', linewidth=0.5)
-    plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
+    plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)   # Dodanie siatki pomocniczej
 
-    # Create a legend
-    legend_labels = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=label)
+
+    legend_labels = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10, label=label) #Jest to lista obiektów Line2D, reprezentujących punkty w legendzie.
                      for label, color in color_map.items()]
-    plt.legend(handles=legend_labels, title='Secondary Structure', loc='upper right')
-    plt.savefig('ramachandran_plot.pdf')
-    plt.show()
+    plt.legend(handles=legend_labels, title='Secondary Structure', loc='upper right') #Tworzy legendę na podstawie utworzonych wcześniej obiektów Line2D.
+    plt.savefig('ramachandran_plot.pdf')  #zapisanie
+    plt.show()                            #wyświetlenie
 
 if __name__ == "__main__":
-    pdb_id = "1HBB"
-    pdb_filename = f"{pdb_id.lower()}.pdb"
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <pdb_file_path>")
+        sys.exit(1)
 
-    parser = PDB.PDBParser(QUIET=True)
+    pdb_filename = sys.argv[1]
 
-    structure = parser.get_structure(pdb_id, pdb_filename)
+    parser = PDB.PDBParser(QUIET=True)  # utworzenie instancji parsera PDB z biblioteki BioPython
 
-    phi_angles, psi_angles, secondary_structure = calculate_phi_psi(structure, pdb_filename)
+    try:
+        structure = parser.get_structure("structure", pdb_filename) # uzyskania obiektu reprezentującego strukturę białka z pliku PDB 
+    except FileNotFoundError:
+        print(f"File {pdb_filename} not found.")
+        sys.exit(1)
+
+    phi_angles, psi_angles, secondary_structure = calculate_phi_psi(structure)
+    #phi_angles: Lista zawierająca kąty phi (ϕ) w stopniach dla poszczególnych reszt aminokwasowych w strukturze białka.
+
+    #psi_angles: Lista zawierająca kąty psi (ψ) w stopniach dla poszczególnych reszt aminokwasowych w strukturze białka.
+
+    #secondary_structure: Lista zawierająca informacje o strukturze wtórnej dla poszczególnych reszt aminokwasowych w formie oznaczeń typów struktury wtórnej (np. helisa, arkusz beta, zgięcie itp.).
 
     plot_ramachandran(phi_angles, psi_angles, secondary_structure)
